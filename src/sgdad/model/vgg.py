f45de081ff006650e707c18bd2092eaf21ae4e79
simonguiroy <guiroysi@elisa1.iro.umontreal.ca>
2018-08-24 16:04:48 -0400

    adding VGG models

---
+++ src/sgdad/model/vgg.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import math
+
+
+__all__ = [
+    'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
+    'vgg19_bn', 'vgg19',
+]
+
+
+# Checkpoints of models pre-trained on Imagenet.
+# NOT SUPPORTED YET
+model_urls = {
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+    'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+    'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+}
+
+
+# Possible configuration variants of VGG network
+cfg = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+
+    def __init__(self, vgg_name='vgg11', dataset='cifar10', init_weights=True, batch_norm=False):
+        super(VGG, self).__init__()
+        self.dataset = dataset
+        self.features = self.make_layers(cfg[vgg_name], self.dataset, batch_norm)
+        
+        if self.dataset == 'cifar10' or self.dataset == 'mnist' or self.dataset == 'fashionmnist':
+            self.classifier = nn.Linear(512, 10)
+            
+        elif self.dataset == 'cifar100':
+            self.classifier = nn.Linear(512, 100)
+            
+        elif self.dataset == 'imagenet':
+            self.classifier = nn.Sequential(
+                nn.Linear(512 * 7 * 7, 4096),
+                nn.ReLU(True),
+                nn.Dropout(),
+                nn.Linear(4096, 4096),
+                nn.ReLU(True),
+                nn.Dropout(),
+                nn.Linear(4096, num_classes),
+            )
+            
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.constant(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant(m.weight, 1)
+                nn.init.constant(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal(m.weight, 0, 0.01)
+                nn.init.constant(m.bias, 0)
+
+
+    def make_layers(self, cfg, dataset, batch_norm):
+        layers = []
+        if dataset == 'mnist' or dataset == 'fashionmnist':
+            in_channels = 1
+        elif dataset == 'cifar10' or dataset == 'cifar100' or dataset == 'imagenet':
+            in_channels = 3
+        
+        for v in cfg:
+            if v == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                if batch_norm:
+                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+                else:
+                    layers += [conv2d, nn.ReLU(inplace=True)]
+                in_channels = v
+        return nn.Sequential(*layers)
+
+
