7a6b9ed649c9979994477122ec23c3ea1b4479fc
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-08-30 15:12:00 -0400

    Add tox for flake8

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from os import path
+#!/usr/bin/env python
 from io import open
-from setuptools import setup, find_namespace_packages
+from os import path
+
+from setuptools import find_namespace_packages, setup
+
 
 HERE = path.abspath(path.dirname(__file__))
 
+
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read().strip()
+
 
 setup(
     name='sgdad',
@@ -15,6 +20,8 @@ setup(
     package_dir={'': 'src'},
     packages=find_namespace_packages('src'),
     install_requires=['torch', 'torchvision', 'pytorch-ignite', 'orion.core'],   # 'kleio.core'],
+    setup_requires=['setuptools', 'pytest-runner'],
+    tests_require=['pytest'],
     # "Zipped eggs don't play nicely with namespace packaging"
     # from https://github.com/pypa/sample-namespace-packages
     zip_safe=False
