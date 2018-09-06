c988c02eb3ca0013968cc0ea7700bc23759da940
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-09-06 18:14:47 -0400

    Fix save python script path

--- bin/1.synthetic/zoo/save.py
+++ bin/1.synthetic/zoo/save.py
@@ -10,7 +10,7 @@ EXPERIMENT = "synthetic"
 kleio_template = 'kleio save --config /config/kleio.core/kleio_config.yaml --tags {experiment};{dataset};{model};{version}'
 
 script_template = (
-    "python3.6 src/sgdad/train.py --config={file_path} "
+    "python3.6 /repos/sgd-space/src/sgdad/train.py --config={file_path} "
     "--model-seed 1 --sampler-seed 1 --epochs 300 "
     "--update data.wrapper.level={data_wrapper_level}")
 
