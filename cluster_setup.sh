0d4facae06ee2bfb71346e3a1e881f587bc3f49a
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-09-08 12:54:27 -0400

    Automate more cluster setup

--- cluster_setup.sh
+++ cluster_setup.sh
@@ -10,21 +10,8 @@
 #     mkdir -p ${INSTALL_DIR}
 # fi
 # 
-pip install --user sregistry[registry] == 0.0.69
-pip install --user git+https://github.com/bouthilx/flow.git
-
 if [ "$CLUSTER" = "cedar" ]
 then
-
-cat << EOF
-Add this into your .bashrc, taking care of replacing all <values>.
-
-\`\`\`
-module load singularity/2.5
-module load 'nixpkgs/16.09' 'python/3.5'
-\`\`\`
-EOF
-
     # Do nothing
 elif [ "$CLUSTER" = "graham" ]
 then
@@ -32,42 +19,65 @@ then
 else
     echo "unknown cluster; trying to install singularity (will fail if user is not root)"
     ./install_singularity.sh 2.5.2 /usr/local
+
+    pip install --user sregistry[registry] == 0.0.69
     exit 1
 fi
 
+
+if [ ! -f $HOME/.sgdspacerc ]; then
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+    cp $DIR/.sgdspacerc $HOME/.sgdspacerc
+    echo "Added \$HOME/.sgdspacerc"
+fi
+
+if grep -Fxq "source .sgdspacerc" $HOME/.bashrc 
+then
+    echo "bashrc already contains source .sgdspacerc"
+else
+    echo "source .sgdspacerc" >> $HOME/.bashrc
+    echo "Added source .sgdspacerc to \$HOME/.bashrc"
+fi
+
+echo "Sourcing $HOME/.sgdspacerc"
+source $HOME/.sgdspacerc
+
+for path in SREGISTRY_STORAGE SINGULARITY_CACHEDIR SREGISTRY_DATABASE CONTAINER_DATA CERTIFICATE_FOLDER CONTAINER_HOME CONTAINER_CONFIG
+do
+
+    if [ ! -d "$path" ]
+    then
+        echo "creating $path"
+        mkdir -p $path
+    fi
+
+done
+
+echo "\n\n"
+
 cat << EOF
-Add this into your .bashrc, taking care of replacing all <values>.
+Set Orion configs in 
+
+\${HOME}/.config/orion.core/orion_config.yaml
+
+Something like
 
 \`\`\`
-SINGULARITY_DIR=</some/path/to/singularity>
-export SREGISTRY_STORAGE=\$SINGULARITY_DIR
-export SINGULARITY_CACHEDIR=\$SINGULARITY_DIR/cache
-export SREGISTRY_DATABASE=\$SINGULARITY_DIR
-export SREGISTRY_NVIDIA_TOKEN=<SECRET>
-
-export CONTAINER_DATA=</some/path/to/data>
-export CONTAINER_HOME=</some/path/to/container/home>
-export CONTAINER_CONFIG=</some/path/to/container/.config>
+database:
+    type: 'mongodb'
+    name: '<DATABASE>'
+    host: 'mongodb://<USER>:<PASSWORD>@<HOST>:<PORT>/<DATABASE>?ssl=true&ssl_ca_certs=<CERTIFICATE_PATH>&authSource=<USER>'
 \`\`\`
 
-If you use mongodb with a certificate file, also add this in your .bashrc
-
-\`\`\`
-export CERTIFICATE_FOLDER=</some/path/to/certs>
-\`\`\`
-
-
-Maybe also consider
-
-\`\`\`
-export PYTHONUNBUFFERED=1
-\`\`\`
 EOF
 
 read -n 1 -s -r -p "Press any key to continue when done"
 
+mkdir ${HOME}/.config/kleio.core
+cp ${HOME}/.config/orion.core/orion_config.yaml ${HOME}/.config/kleio.core/kleio_config.yaml
+
 cat << EOF
-Set Orion configs in 
+Set Orion configs in
 
 \${CONTAINER_CONFIG}/orion.core/orion_config.yaml
 
@@ -80,15 +90,16 @@ database:
     host: 'mongodb://<USER>:<PASSWORD>@<HOST>:<PORT>/<DATABASE>?ssl=true&ssl_ca_certs=/certs/<CERTIFICATE_NAME>&authSource=<USER>'
 \`\`\`
 
-Set Kleio configs in 
-
-\${CONTAINER_CONFIG}/kleio.core/kleio_config.yaml
-
-Same as for Orion
+!! Note that the ssl_ca_certs should start with /certs/ as this is the expected path from within the
+container
 EOF
 
 read -n 1 -s -r -p "Press any key to continue when done"
 
+mkdir ${CONTAINER_CONFIG}/kleio.core
+cp ${CONTAINER_CONFIG}/orion.core/orion_config.yaml ${CONTAINER_CONFIG}/kleio.core/kleio_config.yaml
+
+pip install --user git+https://github.com/bouthilx/flow.git
 
 # cd ${INSTALL_DIR}
     # git clone https://github.com/singularityhub/sregistry-cli
