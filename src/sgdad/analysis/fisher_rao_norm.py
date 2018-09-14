145440a7108bfeff693392d7939055e6455c29e0
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-09-14 16:24:40 -0400

    Push preliminary version of fisher-rao norm
    Why:
    
    To share with Cesar ^_^

--- src/sgdad/analysis/fisher_rao_norm.py
+++ src/sgdad/analysis/fisher_rao_norm.py
@@ -15,5 +15,52 @@ def build():
     return
 
 
-
 # Compute the expectation on the entire set...
+
+
+def build():
+    return ComputeFisherRaoNorm()
+
+
+TOO_COMPLEX_MODEL_ERROR = """\
+Simplified Fisher-Rao norm can only be computed on fully connected layers with ReLU activations and
+no biases
+"""
+
+
+class ComputeFisherRaoNorm(object):
+    def __init__(self):
+        pass
+
+    def __call__(self, results, name, set_name, loader, model, optimizer, device):
+
+        # Dt() define below eq (3.4)
+
+        with torch.no_grads():
+            fisher_rao_norms = self.compute_fisher_rao_norm(loader, model, device)
+
+        return fisher_rao_norms
+
+    def compute_fisher_rao_norm(self, loader, model, device):
+
+        expectation = 0.0
+        emp_expectation = 0.0
+
+        for batch_idx, (data, target) in enumerate(loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            softmax = torch.nn.softmax(output)
+
+            torch.dot(softmax, output) - softmax
+
+        return spectral_norm
+
+    def compute_module_spectral_norm(self, matrix):
+
+        return 1.0  # TODO
+
+    def verify_model(self, model):
+        for module in model.named_modules():
+            if getattr(module, 'bias', None) is not None:
+                raise RuntimeError(TOO_COMPLEX_MODEL_ERROR)
