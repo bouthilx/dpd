cedf2eea9ab291130878abba3a39431d328281fb
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-09-14 19:43:47 -0400

    Add MLPs without biases
    Why:
    
    The metric of the Fisher-Rao norm as proposed by Liang et al. is only
    applicable to fully connected layers with ReLU non linearity and no
    biases. We need at least a few such models to compare our metrics with
    the Fisher-Rao norm.

--- src/sgdad/model/mlp.py
+++ src/sgdad/model/mlp.py
@@ -9,14 +9,14 @@ import torch.nn as nn
 # MLP with two hidden layers, each with 200 neurons, and ReLU activations.
 
 class MLP(nn.Module):
-    def __init__(self, input_size=28*28, num_classes=10, layers=tuple()):
+    def __init__(self, input_size=28*28, num_classes=10, layers=tuple(), bias=True):
         self.architecture = 'MLP--2x200_neurons--input_size=' + str(input_size) + '--num_classes=' + str(num_classes)
         self.input_size = input_size
         super(MLP, self).__init__()
         insizes = [input_size] + list(layers)
         outsizes = list(layers) + [num_classes]
         for i, [insize, outsize] in enumerate(zip(insizes, outsizes)):
-            setattr(self, 'fc{}'.format(i), nn.Linear(insize, outsize))
+            setattr(self, 'fc{}'.format(i), nn.Linear(insize, outsize, bias=bias))
 
     def forward(self, x):
         x = x.view(x.size(0), self.input_size)
@@ -27,8 +27,8 @@ class MLP(nn.Module):
         return layers[-1][1](x)
 
 
-def build(input_size, num_classes, layers):
+def build(input_size, num_classes, layers, bias):
     if isinstance(input_size, list):
         input_size = numpy.product(input_size)
 
-    return MLP(input_size=input_size, num_classes=num_classes, layers=layers)
+    return MLP(input_size=input_size, num_classes=num_classes, layers=layers, bias=bias)
