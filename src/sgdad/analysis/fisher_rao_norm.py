7835d27539321fd6123fdda96f02f8a415175a5b
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-09-15 15:37:00 -0400

    Add limited fisher-rao-norm

--- src/sgdad/analysis/fisher_rao_norm.py
+++ src/sgdad/analysis/fisher_rao_norm.py
@@ -12,13 +12,6 @@
 
 
 def build():
-    return
-
-
-# Compute the expectation on the entire set...
-
-
-def build():
     return ComputeFisherRaoNorm()
 
 
@@ -34,8 +27,6 @@ class ComputeFisherRaoNorm(object):
 
     def __call__(self, results, name, set_name, loader, model, optimizer, device):
 
-        # Dt() define below eq (3.4)
-
         with torch.no_grads():
             fisher_rao_norms = self.compute_fisher_rao_norm(loader, model, device)
 
@@ -43,8 +34,12 @@ class ComputeFisherRaoNorm(object):
 
     def compute_fisher_rao_norm(self, loader, model, device):
 
+        # Raise RuntimeError if analysis cannot be executed on such model architecture.
+        self.verify_model(model)
+
         expectation = 0.0
         emp_expectation = 0.0
+        number_of_samples = 0.0
 
         for batch_idx, (data, target) in enumerate(loader):
             data, target = data.to(device), target.to(device)
@@ -52,15 +47,35 @@ class ComputeFisherRaoNorm(object):
             output = model(data)
             softmax = torch.nn.softmax(output)
 
-            torch.dot(softmax, output) - softmax
+            expectation += self.true_fisher_rao_norm(output, softmax)
+            emp_expectation += self.empirical_fisher_rao_norm(output, softmax, target)
+            number_of_samples += data.size(0)
 
-        return spectral_norm
+        number_of_layers = len(model.named_modules())
+        coefficient = (number_of_layers + 1) ** 2 / number_of_samples
+            
+        return OrderedDict((('fisher_rao_norm.true', coefficient * expectation),
+                            ('fisher_rao_norm.empirical', coefficient * emp_expectation)))
 
-    def compute_module_spectral_norm(self, matrix):
+    def true_fisher_rao_norm(self, output, softmax):
+        # (b,m)-dim
+        diff = torch.dot(softmax.t(), output) - ouput
+        return (softmax * (diff * diff)).sum()
 
-        return 1.0  # TODO
+    def empirical_fisher_rao_norm(self, output, softmax, target):
+        # (b,1)-dim
+        diff = torch.dot(softmax.t(), output) - ouput[target]
+        return (diff * diff).sum()
 
     def verify_model(self, model):
+
+        def is_not_supported(module):
+            has_bias = getattr(module, 'bias', None) is not None
+            has_conv = module.__class__.__name__ == "Conv2d"
+            # Bah, there is nothing else than ReLU in our models, lets be lazy.
+            has_non_relu = False
+            return has_bias or has_conv or has_non_relu
+
         for module in model.named_modules():
-            if getattr(module, 'bias', None) is not None:
+            if is_not_supported(module):
                 raise RuntimeError(TOO_COMPLEX_MODEL_ERROR)
