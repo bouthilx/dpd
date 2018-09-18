83c3913cb9dc118eee9d52656033b6500bfad9cc
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-09-18 08:24:33 -0700

    Add analysis/beta-v1.0 script

--- bin/1.synthetic/analyze/versions/beta-v1.0.sh
+++ bin/1.synthetic/analyze/versions/beta-v1.0.sh
@@ -7,40 +7,40 @@
 FILENAME="$(basename "$0")"
 ANALYSIS_VERSION="a-${FILENAME%.*}"
 EXECUTION_VERSION="beta-v1.0"
-CONTAINER="37b75ff"
+CONTAINER="62351cf"
 
 
 # Simplified Fisher-Rao norm
 ANALYSIS_CONF=configs/1.synthetic/analyzes/fisher_rao_norm.yaml
-./bin/1.synthetic/analyze/sumbit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
+./bin/1.synthetic/analyze/submit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
     --models mlp1wb mlp2wb mlp5wb --epochs 300
     
 
 # KFAC Fisher-Rao norm
 ANALYSIS_CONF=configs/1.synthetic/analyzes/kfac_fisher_rao_norm.yaml
-./bin/1.synthetic/analyze/sumbit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
+./bin/1.synthetic/analyze/submit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
     --epochs 300
     
 
 # Participation ratio
 ANALYSIS_CONF=configs/1.synthetic/analyzes/participation_ratio.yaml
-./bin/1.synthetic/analyze/sumbit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
+./bin/1.synthetic/analyze/submit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
     --models mlp1 mlp2 mlp1wb mlp2wb --epochs 300
     
 
 # Block Diagonal Participation ratio
 ANALYSIS_CONF=configs/1.synthetic/analyzes/bd_participation_ratio.yaml
-./bin/1.synthetic/analyze/sumbit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
+./bin/1.synthetic/analyze/submit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
     --epochs 300
 
 
-# Spectral norm
-ANALYSIS_CONF=configs/1.synthetic/analyzes/spectral_norm.yaml
-./bin/1.synthetic/analyze/sumbit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
-    --epochs 300
+# # Spectral norm
+# ANALYSIS_CONF=configs/1.synthetic/analyzes/spectral_norm.yaml
+# ./bin/1.synthetic/analyze/submit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
+#     --epochs 300
     
 
 # l2 norm
 ANALYSIS_CONF=configs/1.synthetic/analyzes/l2_norm.yaml
-./bin/1.synthetic/analyze/sumbit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
+./bin/1.synthetic/analyze/submit.sh $CONTAINER ${EXECUTION_VERSION} ${ANALYSIS_VERSION} ${ANALYSIS_CONF} \
     --epochs 300
