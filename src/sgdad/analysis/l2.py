efd654566a5c01019d31c98b8ae07d48971dd3a8
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-09-21 13:44:09 -0400

    Add l2-norm with products

--- src/sgdad/analysis/l2.py
+++ src/sgdad/analysis/l2.py
@@ -10,9 +10,12 @@ def build():
 
 def l2_norm(results, name, set_name, loader, model, optimizer, device):
     l2_norm_sum = 0.0
+    l2_norm_prod = 1.0
     for name, module in model.named_modules():
         if hasattr(module, 'weight'):
             squared_w = module.weight * module.weight
             l2_norm_sum += squared_w.sum()
+            l2_norm_prod *= squared_w.sum()
 
-    return OrderedDict((('l2_norm', l2_norm_sum.item()), ))
+    return {'parameters': {'l2_norm': {'sum': l2_norm_sum.item(),
+                                       'prod': l2_norm_prod.item()}}}
