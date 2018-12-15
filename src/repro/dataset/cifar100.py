59c085002c732e460a50a27ac78f6fda7ef2d403
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-12-15 00:09:52 -0500

    Add CIFAR100

---
+++ src/repro/dataset/cifar100.py
@@ -0,0 +1,36 @@
+from collections import OrderedDict
+
+import torch
+from torchvision import datasets, transforms
+
+
+DATA_SIZE = 45000
+
+
+def build(batch_size, data_path, num_workers):
+    transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = datasets.CIFAR100(root=data_path, train=True,
+                                 download=True, transform=transform)
+
+    sampler = torch.utils.data.sampler.SubsetRandomSampler(range(min(DATA_SIZE, len(trainset))))
+
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                               sampler=sampler, num_workers=num_workers)
+
+    sampler = torch.utils.data.sampler.SubsetRandomSampler(range(DATA_SIZE, len(trainset)))
+
+    valid_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                               sampler=sampler, num_workers=num_workers)
+
+    testset = datasets.CIFAR100(root=data_path, train=False,
+                                download=True, transform=transform)
+
+    sampler = torch.utils.data.sampler.SubsetRandomSampler(range(min(DATA_SIZE, len(testset))))
+
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                              sampler=sampler, num_workers=num_workers)
+
+    return OrderedDict(train=train_loader, valid=valid_loader, test=test_loader)
