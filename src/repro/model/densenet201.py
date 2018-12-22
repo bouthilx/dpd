576111116e81ef843a3c4015edf44e2e3aab7ec1
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-12-22 14:10:50 -0500

    Change DenseNet to torchvision version

--- src/repro/model/densenet201.py
+++ src/repro/model/densenet201.py
@@ -2,6 +2,6 @@ from repro.model.densenet import DenseNet, distribute
 
 
 def build(input_size, num_classes, distributed=0):
-    model = DenseNet(num_init_features=64, growth_rate=32, block_config=(6, 12, 24, 16))
+    model = DenseNet(num_init_features=64, growth_rate=32, block_config=(6, 12, 48, 32))
 
     return distribute(model, distributed)
