af47902b4230be0650740927c916f4a022d1967b
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-12-22 14:37:52 -0500

    Import torch for distributed ResNet

--- src/repro/model/resnet.py
+++ src/repro/model/resnet.py
@@ -1,7 +1,8 @@
-import torch.nn as nn
 import math
-import torch.utils.model_zoo as model_zoo
 
+import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
 
 
 # All models here are assumed to accept RGB input, thus 3 input channels.
--- src/repro/model/vgg.py
+++ src/repro/model/vgg.py
@@ -1,7 +1,8 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
-import math
 
 
 __all__ = [
