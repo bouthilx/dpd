e8392d5a3bfbbcd0c09c3153fb8936175b78747d
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-01-30 14:15:14 -0500

    Adapt LeNet for TinyImageNet
    Why:
    
    The input size is larger for ImageNet and the final fully-connected
    layers of LeNet do not have the right shape.
    
    How:
    
    Adapt architecture based on input size so that input to fully connected
    layers stays relatively the same.

--- src/repro/model/lenet.py
+++ src/repro/model/lenet.py
@@ -5,24 +5,35 @@ class LeNet(nn.Module):
     def __init__(self, input_size, num_classes):
         super(LeNet, self).__init__()
         n_channels = input_size[0]
-        if n_channels == 1:
+        if tuple(input_size) == (1, 28, 28):
             self.conv1 = nn.Conv2d(n_channels, 20, 5, 1)
+            self.pool1 = nn.MaxPool2d(2, 2)
             self.conv2 = nn.Conv2d(20, 50, 5, 1)
+            self.pool2 = nn.MaxPool2d(2, 2)
             self.fc1   = nn.Linear(50 * 4 * 4, 500)
             self.fc2   = nn.Linear(500, num_classes)
+        elif tuple(input_size) == (3, 32, 32):
+            self.conv1 = nn.Conv2d(n_channels, 20, 5, 1)
+            self.pool1 = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(20, 50, 5, 1)
+            self.pool2 = nn.MaxPool2d(2, 2)
+            self.fc1   = nn.Linear(50 * 5 * 5, 500)
+            self.fc2   = nn.Linear(500, num_classes)
+        elif tuple(input_size) == (3, 64, 64):
+            self.conv1 = nn.Conv2d(n_channels, 20, 5, 1)
+            self.pool1 = nn.MaxPool2d(3, 3)
+            self.conv2 = nn.Conv2d(20, 50, 5, 1)
+            self.pool2 = nn.MaxPool2d(3, 3)
+            self.fc1   = nn.Linear(50 * 5 * 5, 500)
+            self.fc2   = nn.Linear(500, num_classes)
         else:
-            self.conv1 = nn.Conv2d(n_channels, 6, 5, 1)
-            self.conv2 = nn.Conv2d(6, 16, 5, 1)
-            self.fc1   = nn.Linear(16 * 5 * 5, 120)
-            self.fc2   = nn.Linear(120, num_classes)
-
-        # self._initialize_weights()
+            raise RuntimeError("No architecture for input size: {}".format(input_size))
 
     def forward(self, x):
         out = nn.functional.relu(self.conv1(x))
-        out = nn.functional.max_pool2d(out, 2, 2)
+        out = self.pool1(out)
         out = nn.functional.relu(self.conv2(out))
-        out = nn.functional.max_pool2d(out, 2, 2)
+        out = self.pool2(out)
         out = out.view(out.size(0), -1)
         out = nn.functional.relu(self.fc1(out))
         out = self.fc2(out)
