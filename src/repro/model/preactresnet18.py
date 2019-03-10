c917942661b99126b24a124255ccc3264704774a
Thrandis <cesarlaurent77@gmail.com>
2019-03-10 19:49:30 +0100

    Updated preactresnet for tinyimagenet.

--- src/repro/model/preactresnet18.py
+++ src/repro/model/preactresnet18.py
@@ -1,5 +1,7 @@
 from repro.model.preactresnet import PreActBlock, PreActResNet
 
 
-def build(input_size, num_classes):
-    return PreActResNet(PreActBlock, [2,2,2,2], input_size=input_size, num_classes=num_classes)
+def build(input_size, conv, maxpool, avgpool, num_classes):
+    return PreActResNet(PreActBlock, [2,2,2,2], input_size=input_size,
+                        conv=conv, maxpool=maxpool, avgpool=avgpool,
+                        num_classes=num_classes)
