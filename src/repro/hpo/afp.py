3b5d612e6b2ad20df56c6d0f359a0eae2901e987
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-04-15 15:51:44 -0400

    Finish impl of AFP

--- src/repro/hpo/afp.py
+++ src/repro/hpo/afp.py
@@ -16,10 +16,6 @@ except ImportError:
     SignalSuspend = None
 
 
-# TODO: Add metrics to mahler tasks
-#       Test with lenet
-
-
 def removeOutliers(x, outlierConstant):
     a = numpy.array(x)
     upper_quartile = numpy.percentile(a, 75, axis=1)
@@ -38,34 +34,39 @@ def removeOutliers(x, outlierConstant):
 class AsynchronousFilteringPercentile:
 
     def __init__(self, initial_population, final_population, n_steps=30, window_size=11,
-                 n_points=200, min_population=3):
+                 n_points=200, min_population=3, population_growth_brake=1):
+
         self.initial_population = initial_population
         self.final_population = final_population
         self.n_steps = n_steps
         self.window_size = window_size
         self.padding = int(window_size / 2)
-        ratio = (final_population / initial_population) ** (1 / n_steps)
-        self.percentile = int(ratio * 100 + 0.5)
+        self.ratio = (final_population / initial_population) ** (1 / n_steps)
+        self.percentile = int(self.ratio * 100 + 0.5)
         print('Percentile:', self.percentile)
         self.min_population = min_population
+        self.population_growth_brake = population_growth_brake
 
         self.trials = {}
+        self.first_epoch = None
+        self.epochs_buffer = None
+         
         # self.metrics = numpy.zeros((2000, 300))
-        self.metrics = numpy.ones((initial_population, n_points + 1)) * -1
-        self.smooth_metrics = numpy.ones((initial_population, n_points + 1)) * -1
+        self.metrics = numpy.ones((int(initial_population * 1.1), n_points + 1)) * -1
+        self.smooth_metrics = numpy.ones((int(initial_population * 1.1), n_points + 1)) * -1
 
         self.task_timestamp = None
         self.metric_timestamp = None
 
-        self.task_id = None
+        self.task = None
         if mahler is not None:
-            self.task_id = mahler.get_current_task_id()
-            if self.task_id is not None:
+            task_id = mahler.get_current_task_id()
+            if task_id is not None:
                 self.mahler_client = mahler.Client()
-                task = list(self.mahler_client.find(id=self.task_id))[0]
-                self.tags = task.tags
+                self.task = list(self.mahler_client.find(id=task_id))[0]
+                self.tags = self.task.tags
 
-        if self.task_id is None:
+        if self.task is None:
             print('WARNING: Cannot use MedianDistanceStoppingRule without mahler.')
 
     def get_decisive_steps(self):
@@ -77,8 +78,8 @@ class AsynchronousFilteringPercentile:
         mask *= (n_points >= self.final_population)
 
         # print(var)
-        diff = (numpy.max(self.metrics * mask, axis=0) -
-                numpy.min(self.metrics * mask, axis=0))
+        diff = (numpy.max(self.smooth_metrics * mask, axis=0) -
+                numpy.min(self.smooth_metrics * mask, axis=0))
 
         # Compute the cummulative integral of variance over epochs
         # and pick a list of epochs so that all intervals contain the same
@@ -98,7 +99,6 @@ class AsynchronousFilteringPercentile:
         while len(steps) < self.n_steps:
             steps.append(steps[-1] + 1)
 
-        print(steps)
         return steps
 
     @property
@@ -107,15 +107,84 @@ class AsynchronousFilteringPercentile:
 
     def close(self):
         self.mahler_client.close()
+        
+    def update_epoch_buffer(self, epoch):
+        if self.epochs_buffer:
+            return
+
+        if self.first_epoch is None:
+            self.first_epoch = epoch
+        
+        # We assume events we first find are running, since the task is actually executing this part
+        # of the code.
+        last_event = None
+        for event in self.task._status.events[::-1]:
+            if event['item']['name'] != 'Running':
+                break
+            last_event = event
+
+        if last_event is None:
+            return
+
+        start_time = last_event['id'].generation_time
+
+        second_epoch = None
+        first_epoch = None
+        for metric_event in self.task._metrics.events[::-1]:
+            # Stop if we passed the current execution time.
+            if metric_event['id'].generation_time < start_time:
+                break
+
+            # Ignore metrics like usage.
+            if metric_event['item']['type'] != 'stat':
+                continue
+
+            second_epoch = first_epoch
+            first_epoch = metric_event['id'].generation_time
+        
+        if second_epoch is None:
+            return
+
+        time_per_epoch = second_epoch - first_epoch
+        # Removing time_per_epoch because overhead counts time until beginning of first epoch, not
+        # end of first epoch.
+        overhead = ((first_epoch - start_time) - time_per_epoch).total_seconds()
+        time_per_epoch = time_per_epoch.total_seconds()
+        self.epochs_buffer = max(int(overhead / time_per_epoch + 0.5), 1)
+        print('Estimating overhead and compute time per epochs.')
+        print('         Started on:', start_time)
+        print(' End of first epoch:', first_epoch) 
+        print('End of second epoch:', second_epoch) 
+        print('           Overhead:', overhead)
+        print('    Time per epochs:', time_per_epoch)
+        print('   Estimated buffer:', self.epochs_buffer)
+        print('    First epoch was:', self.first_epoch)
+        print('  Won\'t stop before:', self.first_epoch + self.epochs_buffer)
+
+        # started
+        # first metric (overhead)
+        # second metric (time per epochs)
+        #
 
     def update(self, stats):
-        if self.task_id is None:
+        if self.task is None:
             pprint.pprint(stats)
             return
 
-        self.mahler_client.add_metric(self.task_id, stats, force=True)
+        self.mahler_client.add_metric(self.task.id, stats, force=True)
+
+        # This will refresh the trials and metrics at the same time.
+        self.thaw(subset=10)
 
         epoch = stats['epoch']
+        self.update_epoch_buffer(epoch)
+
+        if self.epochs_buffer is None:
+            return
+
+        if epoch < self.first_epoch + self.epochs_buffer:
+            return
+
         # if epoch < self.grace:
         #     return
         print("Should I skip?")
@@ -125,50 +194,35 @@ class AsynchronousFilteringPercentile:
         if epoch < decisive_epochs[0]:
             return
 
-        # This will refresh the trials and metrics at the same time.
-        self.thaw(subset=10)
+        idx = max(bisect.bisect(decisive_epochs, epoch) - 1, 0)
+        decisive_epoch = decisive_epochs[idx]
+        print('decisive_epoch:', decisive_epoch)
 
-        decisive_epochs = self.get_decisive_steps()
+        percentile, population = self.fetch_results(decisive_epoch)
 
-        print(epoch, decisive_epochs)
-
-        if epoch < decisive_epochs[0]:
+        population_at_step = self.initial_population * (self.ratio) ** (idx + 1)
+        print(decisive_epoch, percentile, population, self.final_population, population_at_step)
+        # If population is too small but should soon be fine, use percentile to determine
+        # suspension
+        # Otherwise, suspend until population is large enough.
+        # TODO: Limiting progression to having giving lot of time to bad trials at beginning
+        #       This brake can be adjusted with self.population_growth_brake
+        if population < population_at_step * self.population_growth_brake: #  and not population_is_soon_enough:
+            message = 'Population too small: {} < {} ({} * {} * {} ** {})'.format(
+                population, population_at_step * self.population_growth_brake, 
+                self.population_growth_brake, self.initial_population, self.ratio, idx + 1)
+            raise SignalSuspend(message)
+        elif percentile is None:
             return
 
-        idx = max(bisect.bisect(decisive_epochs, epoch), 1)
-        decisive_epochs = decisive_epochs[:idx]
-
-        last_population = None
-
-        for i, decisive_epoch in enumerate(decisive_epochs):
-            percentile, population = self.fetch_results(decisive_epoch)
-
-            population_is_soon_enough = (
-                (i == 0) or
-                (last_population is not None and last_population >= self.final_population))
-            print(i, percentile, population, last_population, self.final_population)
-            # If population is too small but should soon be fine, use percentile to determine
-            # suspension
-            # Otherwise, suspend until population is large enough.
-            if population < self.final_population and not population_is_soon_enough:
-                message = 'Population too small: {} < {}'.format(population, self.final_population)
-                raise SignalSuspend(message)
-            # There is not enough samples to compute the percentile and population is soon enough...
-            # huh, is that possible? TODO
-            elif percentile is None:
-                continue
-
-            # TODO: Turn percentile down when population is to small, so very good candidates are
-            # allowed to continue
-
-            decisive_value = self.smooth_metrics[self.trials[self.task_id], decisive_epoch]
-            print(decisive_value)
-            if decisive_value > percentile:
-                message = 'value({}) > percentile({}) at epoch({}) with population({})'.format(
-                    decisive_value, percentile, decisive_epoch, population)
-                raise SignalSuspend(message)
-
-            last_population = population
+        print(self.metrics[self.trials[str(self.task.id)], decisive_epoch - 3:decisive_epoch + 4])
+        print(self.smooth_metrics[self.trials[str(self.task.id)], decisive_epoch - 3:decisive_epoch + 4])
+        decisive_value = self.smooth_metrics[self.trials[str(self.task.id)], decisive_epoch]
+        print(decisive_value)
+        if decisive_value > percentile:
+            message = 'value({}) > percentile({}) at epoch({}) with population({})'.format(
+                decisive_value, percentile, decisive_epoch, population)
+            raise SignalSuspend(message)
 
     def refresh(self):
         self._update_trials()
@@ -222,14 +276,15 @@ class AsynchronousFilteringPercentile:
             # min_values = numpy.minimum.accumulate(self.metrics[metric_key][epoch:])
             # self.metrics[metric_key][epoch:] = min_values
 
-            n_points = (self.metrics[metric_key] >= 0).sum()
-            a = max(epoch - self.padding, 0)
+            # +1 to compensate for missing epoch 0
+            n_points = (self.metrics[metric_key] >= 0).sum() + 1
+            a = max(epoch - self.padding, 1)
             effective_padding = epoch - a
             b = min(epoch + effective_padding + 1, n_points)
 
             # TODO: Compute all this afterwards using vector computations in numpy
             for o_epoch in range(a, b):
-                a = max(o_epoch - self.padding, 0)
+                a = max(o_epoch - self.padding, 1)
                 effective_padding = o_epoch - a
                 b = min(o_epoch + effective_padding + 1, n_points)
 
@@ -253,37 +308,36 @@ class AsynchronousFilteringPercentile:
         return numpy.percentile(self.smooth_metrics[indices, epoch], self.percentile), population
 
     def thaw(self, subset=None):
-        if self.task_id is None:
+        if self.task is None:
             return
 
         self.refresh()
         decisive_epochs = self.get_decisive_steps()
         percentiles = dict()
-        if subset:
-            trial_ids = numpy.random.choice(list(self.trials.keys()), size=subset, replace=False)
+        if subset and len(self.trials) > subset:
+            trial_ids = numpy.random.choice(
+                list(self.trials.keys()), size=subset, replace=False)
         else:
             trial_ids = self.trials.keys()
 
         # TODO: Compute all based on matrix directly and back-track to trial ids.
         for trial_id in trial_ids:
             metric_index = self.trials[trial_id]
-            if trial_id == self.task_id:
+            if trial_id == str(self.task.id):
                 continue
 
             trial_metrics = self.smooth_metrics[metric_index]
-            # - 1 to avoid counting metric before first epoch
-            last_epoch = sum(trial_metrics >= 0) - 1
+            # We can ignore epoch 0 because it is set to -1
+            last_epoch = sum(trial_metrics >= 0)
 
             # if last_epoch < 1:
             #     self._resume(trial_id, 'Should not be suspended before epoch 1')
             #     continue
     
-            idx = max(bisect.bisect(decisive_epochs, last_epoch), 1)
-            decisive_epoch = decisive_epochs[min(idx, len(decisive_epochs)) - 1]
+            idx = max(bisect.bisect(decisive_epochs, last_epoch) - 1, 0)
+            decisive_epoch = decisive_epochs[idx]
 
             if last_epoch < decisive_epoch:
-                # TODO: make sure this is ok. There seems to be a bug as many many gets thawed this
-                # way.
                 message = 'Should not be suspended before epoch {}. (Was {}) ({})'.format(
                     decisive_epoch, last_epoch, str(decisive_epochs))
                 self._resume(trial_id, message)
@@ -299,9 +353,14 @@ class AsynchronousFilteringPercentile:
             percentile, population = percentiles[decisive_epoch]
             # Deliberately avoid = median to limit unstability
             # (if one trial moves around median it would get suspended and resumed very often)
-            if trial_value < percentile and population >= self.final_population:
+            population_at_step = self.initial_population * (self.ratio) ** (idx + 1)
+            enough_population = population >= population_at_step * self.population_growth_brake
+            if trial_value < percentile and enough_population:
+                population_message = '{} >= {} ({} * {} * {} ** {})'.format(
+                    population, population_at_step * self.population_growth_brake, 
+                    self.population_growth_brake, self.initial_population, self.ratio, idx + 1)
                 message = 'value({}) < percentile({}) at epoch({}) with population({})'.format(
-                    trial_value, percentile, decisive_epoch, population)
+                    trial_value, percentile, decisive_epoch, population_message)
                 self._resume(trial_id, message)
 
     def _resume(self, trial_id, message):
