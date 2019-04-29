add1ac68a36d44417e77766c61f1c73fc4cb174b
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-04-29 14:02:46 -0400

    Adapt configurators to new API

--- src/repro/hpo/configurator/bayesopt.py
+++ src/repro/hpo/configurator/bayesopt.py
@@ -14,36 +14,18 @@ logger = logging.getLogger(__name__)
 
 
 class BayesianOptimizer:
-    def __init__(self, space, max_trials, **kwargs):
+    def __init__(self, space, max_trials, seed, **kwargs):
         self.primary = PrimaryAlgo(space, {'BayesianOptimizer': kwargs})
+        self.primary.algorithm.random_state = seed
         self.max_trials = max_trials
-        self.trials = []
+        self.trial_count = 0
 
     @property
     def space(self):
         return self.primary.space
 
-    def get_bests(self, max_resource):
-        # Include non completed trials to avoid ignoring them.
-        best_trials = [trial for trial in self.trials
-                       if trial['status'] != 'Completed']
-
-        def validation_error(trial):
-            try:
-                return trial['objective']
-            except KeyError:
-                pprint.pprint(trial)
-                raise
-
-        completed_trials = (trial for trial in self.trials
-                            if trial['status'] == 'Completed' and trial['objective'])
-
-        best_trials += list(sorted(completed_trials, key=validation_error))
-
-        return best_trials[:max_resource]
-
     def is_completed(self):
-        return len(self.trials) >= self.max_trials
+        return self.trial_count >= self.max_trials
 
     def get_params(self, seed=None):
         if seed is None:
@@ -59,22 +41,18 @@ class BayesianOptimizer:
         logger.debug('Sampling:\n{}'.format(pprint.pformat(params)))
         return params
 
-    def observe(self, trials):
-        self.trials += trials
+    def observe(self, params, objective):
 
-        params = []
-        results = []
-        for trial in trials:
-            params.append([trial['params'][param_name] for param_name in self.space.keys()])
-            results.append({'objective': trial['objective']})
+        params = [[params[param_name] for param_name in self.space.keys()]]
+        results = [dict(objective=objective)]
 
         self.primary.observe(params, results)
 
 
 if PrimaryAlgo is not None:
-    def build(space, max_trials, strategy, n_initial_points, acq_func, alpha, n_restarts_optimizer,
+    def build(space, max_trials, seed, strategy, n_initial_points, acq_func, alpha, n_restarts_optimizer,
               noise, normalize_y):
         return BayesianOptimizer(
-            space, max_trials=max_trials, strategy=strategy, n_initial_points=n_initial_points,
+            space, max_trials=max_trials, seed=seed, strategy=strategy, n_initial_points=n_initial_points,
             acq_func=acq_func, alpha=alpha, n_restarts_optimizer=n_restarts_optimizer,
             noise=noise, normalize_y=normalize_y)
--- src/repro/hpo/configurator/random_search.py
+++ src/repro/hpo/configurator/random_search.py
@@ -8,40 +8,22 @@ from repro.utils.flatten import unflatten
 logger = logging.getLogger(__name__)
 
 
-def build(space, max_trials):
-    return RandomSearch(space, max_trials)
+def build(space, max_trials, seed):
+    return RandomSearch(space, max_trials, seed)
 
 
 class RandomSearch(object):
-    def __init__(self, space, max_trials):
+    def __init__(self, space, max_trials, seed=1):
         self.space = space
         self.max_trials = max_trials
+        self.seed = seed
         self.trials = []
-
-    def get_bests(self, max_resource):
-        # Include non completed trials to avoid ignoring them.
-        best_trials = [trial for trial in self.trials
-                       if trial['registry']['status'] != 'Completed']
-
-        def validation_error(trial):
-            try:
-                return trial['output']['best']['valid']['error_rate']
-            except KeyError:
-                pprint.pprint(trial)
-                raise
-
-        completed_trials = (trial for trial in self.trials
-                            if trial['registry']['status'] == 'Completed' and trial['output'])
-
-        best_trials += list(sorted(completed_trials, key=validation_error))
-
-        return best_trials[:max_resource]
 
     def is_completed(self):
         return len(self.trials) >= self.max_trials
 
-    def observe(self, trials):
-        self.trials += trials
+    def observe(self, params, objective):
+        self.trials.append((params, objective))
 
     def get_params(self, seed=None):
         if seed is None:
