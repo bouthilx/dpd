dd0eb6602af0e19b18510caaf7caa2f936223742
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-04-29 14:03:32 -0400

    Add missing trial callback

--- src/repro/hpo/dispatcher/trial.py
+++ src/repro/hpo/dispatcher/trial.py
@@ -1,18 +1,36 @@
 import copy
+import functools
+import uuid
 from queue import Empty as EmptyQueueException
 from multiprocessing import Queue, Process
 from typing import Optional, Callable, Dict, Tuple
 
 
+def callback(queue, **kwargs):
+    queue.put(kwargs)
+
+
+# This will be run remotely with Mahler
+def mahler_callback(mahler_config, **kwargs):
+    mahler_client = mahler.Client(**mahler_config)
+    mahler_client.add_metric(kwargs, type='hpo')
+
+    # TODO: For mahler,
+    # Start will register a task in mahler and start a monitoring process
+    # The monitoring procell will listen to task.metrics[hpo] a report anything new to the queue.
+    # Map task status from mahler to Trial interface.
+    
+
 class Trial:
     """ A trial represent an experience in progress, it holds all the necessary information to stop it if it is
         in progress or resume it if it was suspended """
-    def __init__(self, task: Callable[[Dict[str, any]], None], params: Dict[str, any], queue: Queue):
+    def __init__(self, id: str, task: Callable[[Dict[str, any]], None], params: Dict[str, any], queue: Queue):
+        self.id = id
         self.task = task
         self.params = copy.deepcopy(params)
         self.kwargs = params
         self.queue: Queue = queue
-        self.kwargs['queue'] = queue
+        self.kwargs['callback'] = functools.partial(callback, queue=queue)
         self.process: Optional[Process] = None
         self.latest_results = None
 
@@ -56,4 +74,3 @@ class Trial:
 
                 self.latest_results = tuple(obs)
                 return tuple(obs)
-
