a890ad0ca27f59373d562d3c2f67f0b5cb976cd2
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-05-06 10:10:57 -0400

    Change mahler names to avoid conflicts

--- src/repro/hpo/resource/mahler_manager.py
+++ src/repro/hpo/resource/mahler_manager.py
@@ -1,11 +1,35 @@
+import getpass
+import logging
+import os
+import pprint
+import time
 from multiprocessing import Process
 
 try:
-    import mahler
-except ImportError:
+    from fabric import Connection
+except:
+    Connection = None
+
+try:
+    import mahler.core.resources
+except ImportError as e:
+    print(e)
     mahler = None
 
+
 from repro.hpo.resource.builtin import ResourceManager
+
+
+logger = logging.getLogger(__name__)
+
+
+FLOW_OPTIONS_TEMPLATE = "{array}time=2:59:00;job-name={job_name}"
+
+FLOW_TEMPLATE = "flow-submit {container} --root {root_dir} --prolog '{prolog}' --options '{options}'"
+
+COMMAND_TEMPLATE = "mahler execute{container}{tags}{options}"
+
+SUBMIT_COMMANDLINE_TEMPLATE = "{flow} launch {command}"
 
 
 # NOTE: When submitting sections with mahler, need to take into account how many workers each of
@@ -14,18 +38,36 @@ from repro.hpo.resource.builtin import ResourceManager
 # TODO: Support local for debugging and multi-cluster for deployment.
 class MahlerResourceManager(ResourceManager):
 
-    def __init__(self, workers, resources, container, tags):
+    def __init__(self, workers, resources, container, tags, workers_per_job=10, monitoring_interval=300):
         super(MahlerResourceManager, self).__init__(workers, resources)
         self.container = container
         self.tags = tags
+        self.workers_per_job = workers_per_job
+        self.monitoring_interval = monitoring_interval
+        self.schedulers = []
+        assert Connection is not None, 'Fabric must be installed'
+
+        mahler.core.resources.load_config(type='remoteflow')
+        config = mahler.core.config.scheduler.remoteflow
+        self.hosts = config.hosts
+        for host in self.hosts.values():
+            host.setdefault('max_workers', config.max_workers)
+            host.setdefault('submission_root', config.submission_root)
+
+        self.user = getpass.getuser()
 
     def start(self):
         print(f'mahler -v execute --tags {" ".join(self.tags)} --num-workers {self.workers}')
-       
-        for host in self.hosts:
-            scheduler = Scheduler(host)
+
+        for host_name, host in self.hosts.items():
+            scheduler = Scheduler(self.user, host_name, self.workers, self.workers_per_job,
+                                  self.resources,
+                                  host['max_workers'],
+                                  host['prolog'], host['submission_root'],
+                                  container=self.container,
+                                  monitoring_interval=self.monitoring_interval)
             scheduler.start()
-            self.schedulers.append(schedulers)
+            self.schedulers.append(scheduler)
 
     def stop(self):
         for scheduler in self.schedulers:
@@ -33,40 +75,49 @@ class MahlerResourceManager(ResourceManager):
 
             # Duplicate workers on all clusters
             # Count co-workers and dedicated workers
-            
+
             # lauch in parallel a resources manager for each cluster. They will duplicate
             # everything.
 
 
 class Scheduler(Process):
-    def __init__(self, user, host, workers, max_workers, prolog, 
-                 submission_root,
-                 monitoring_interval=300):
+    def __init__(self, user, host, workers, workers_per_job, resources, max_workers, prolog,
+                 submission_root, container, monitoring_interval=300):
+        super(Scheduler, self).__init__()
         self.user = user
         self.host = host
         self.workers = workers
+        self.workers_per_job = workers_per_job
+        self.resources = resources
         self.max_workers = max_workers
+        self.prolog = prolog
         self.submission_root = submission_root
+        self.container = container
         self.monitoring_interval = monitoring_interval
+        self.tags = ['worker']
 
     def init(self):
         """
         """
-        logger.info(f'Initiating ssh connection for {self.user}@{self.host}')
+        logger.info(f'{self.user}@{self.host} Initiating ssh connection')
         self.connection = Connection(self.host, user=self.user)
-        logger.info(f'Pulling {self.container} on logging node of {self.host}')
+        logger.info(f'{self.user}@{self.host} Pulling {self.container} on logging node')
         out = self.connection.run('sregistry pull {}'.format(self.container), hide=True, warn=True)
         logger.info(out.stdout)
         logger.debug(out.stderr)
-        logger.info(f'Pulling finished on {self.host}')
+        logger.info(f'{self.user}@{self.host} Pulling completed')
+
+        if self.submission_root is None:
+            raise ValueError(f"submission_root is not defined for host {self.host} nor globally.")
+        submission_dir = os.path.join(self.submission_root, self.container)
+        logger.info(f'{self.user}@{self.host} Creating `{submission_dir}`')
+        self.connection.run('mkdir -p {}'.format(submission_dir))
 
     def status(self):
         """
         """
         command = 'squeue -r -o "%t %j" -u {user}'.format(user=getpass.getuser())
-        workers = 
-        jobs = {}
-        max_workers = 0
+        workers = {}
         logger.debug(f'squeue on {self.host}')
         out = self.connection.run(command, hide=True, warn=True)
         out = out.stdout
@@ -92,31 +143,31 @@ class Scheduler(Process):
             if name == self.name:
                 workers[state] += 1
 
-        return workers, resources
-
-    def submit(self, n_jobs):
-        tags = ['worker']
-        pass
+        return workers, states
 
     def run(self):
+        self.init()
+
         while True:
             workers, resources = self.status()
-            
-            logger.info(f'Host {self.host} status:')
-            logger.info(f'    workers:')
+
+            logger.info(f'{self.user}@{self.host} status:')
+            logger.info(f'{self.user}@{self.host}    workers:')
             logger.info(pprint.pformat(workers))
-            logger.info(f'    resources:')
+            logger.info(f'{self.user}@{self.host}    resources:')
             logger.info(pprint.pformat(resources))
 
             n_tasks = max(self.workers - sum(workers.values()), 0)
-            available = max(sum(resources.values()) - self.max_workers, 0)
+            available = max(self.max_workers - sum(resources.values()), 0)
             n_tasks = min(n_tasks, available)
 
             if n_tasks:
-                logger.info(f'Submitting {n_tasks} on {self.host}')
+                logger.info(f'{self.user}@{self.host} Submitting {n_tasks}')
                 self.submit(n_tasks)
+            else:
+                logger.info(f'{self.user}@{self.host} No task to submit')
 
-            logger.debug(f'Waiting {self.monitoring_interval} seconds')
+            logger.debug(f'{self.user}@{self.host} Waiting {self.monitoring_interval} seconds')
             time.sleep(self.monitoring_interval)
 
     def stop_gracefully(self):
@@ -125,7 +176,7 @@ class Scheduler(Process):
         # But, the manager should make sure to stop all trials prior leaving.
         pass
 
-    def submit_single_host(self, n_workers):
+    def submit(self, n_workers):
 
         array_option = 'array=1-{};'.format(n_workers)
         flow_options = FLOW_OPTIONS_TEMPLATE.format(
@@ -144,21 +195,15 @@ class Scheduler(Process):
 
         flow_options += ";" + ";".join(resources)
 
-        if self.submission_root is None:
-            raise ValueError(
-                "submission_root is not defined for host {} nor globally.".format(host_name))
-        submission_dir = os.path.join(self.submission_root, container)
-        # TODO: Run mkdirs -p with connection.run instead of python's `os`.
-        #       this folder should be created in _ensure_remote_setup.
-        if not os.path.isdir(submission_dir):
-            connection.run('mkdir -p {}'.format(submission_dir))
+        submission_dir = os.path.join(self.submission_root, self.container)
 
         flow_command = FLOW_TEMPLATE.format(
-            container=container, root_dir=submission_dir, prolog=self.prolog, options=flow_options)
+            container=self.container, root_dir=submission_dir, prolog=self.prolog,
+            options=flow_options)
 
         options = {}
-        if self.num_workers:
-            options['num-workers'] = self.num_workers
+        if self.workers_per_job:
+            options['num-workers'] = self.workers_per_job
 
         if options:
             options = ' ' + ' '.join('--{}={}'.format(k, v) for k, v in options.items())
@@ -172,14 +217,19 @@ class Scheduler(Process):
 
         submit_command = SUBMIT_COMMANDLINE_TEMPLATE.format(flow=flow_command, command=command)
 
-        logger.info(f"Executing on {self.host}:")
-        logger.info(submit_command)
-        out = self.connection.run(submit_command, hide=True, warn=True)
-        logger.info("Command output:")
-        logger.info(out.stdout)
-        logger.info(out.stderr)
+        logger.info(f"{self.user}@{self.host} Executing '{submit_command}'")
+        # out = self.connection.run(submit_command, hide=True, warn=True)
+        # logger.info("Command output:")
+        # logger.info(out.stdout)
+        # logger.info(out.stderr)
 
 
 if mahler:
     def build(workers, resources={}, container=None, tags=tuple()):
         return MahlerResourceManager(workers, resources, container=container, tags=tags)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    manager = MahlerResourceManager(10, {'cpu': 4, 'gpu': 1, 'mem': '10GB'}, container='test',
+                                    tags=['nada'])
