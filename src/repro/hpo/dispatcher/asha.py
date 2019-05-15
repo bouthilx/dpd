1807e5c3272f9a212b79d337cfbd0849ef4fe5b5
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-05-15 09:45:12 -0400

    Implement standard version of ASHA

--- src/repro/hpo/dispatcher/asha.py
+++ src/repro/hpo/dispatcher/asha.py
@@ -1,12 +1,7 @@
-from collections import defaultdict
-import argparse
 import logging
-import pprint
-import random
 
-from typing import Dict
+import numpy
 
-from repro.utils.flatten import flatten, unflatten
 from repro.hpo.dispatcher.dispatcher import HPODispatcher
 
 
@@ -19,48 +14,78 @@ logger = logging.getLogger(__name__)
 # mahler.find(tags, status=None)
 
 
-def build(space, configurator_config, fidelities, reduction_factor, max_resource, max_trials, seed):
-    return ASHA(space, configurator_config, fidelities, reduction_factor, max_resource, max_trials, seed)
+def build(space, configurator_config, max_epochs, grace_period, reduction_factor, brackets,
+          max_trials, seed):
+    return ASHA(space, configurator_config, max_epochs, grace_period, reduction_factor, brackets,
+                max_trials, seed)
 
 
 class ASHA(HPODispatcher):
-    def __init__(self, space, configurator_config, fidelities, reduction_factor, max_resource, max_trials, seed):
+    def __init__(self, space, configurator_config, max_epochs, grace_period, reduction_factor,
+                 brackets, max_trials, seed):
         super(ASHA, self).__init__(space, configurator_config, max_trials, seed)
 
-        self.fidelities = fidelities
-        self.rungs = defaultdict(set)
-        self.max_resource = max_resource
+        self.max_epochs = max_epochs
+        self.grace_period = grace_period
         self.reduction_factor = reduction_factor
-        self.base = len(self.fidelities) - 1
+        self.trial_info = {}  # Stores Trial -> Bracket
 
-    def _observe(self, trial_id, params, step, objective, finished=False, **kwargs):
-        if trial_id not in self.observations:
-            rung_id = 0
-            self.rungs[0].add(trial_id)
+        # Tracks state for new trial add
+        self.brackets = [
+            _Bracket(self, grace_period, max_epochs, reduction_factor, s)
+            for s in range(brackets)
+        ]
 
-        super(ASHA, self)._observe(trial_id, params, step, objective, finished, **kwargs)
+        logger.info(f'brackets: {[bracket.rungs for bracket in self.brackets]}')
 
-    def make_suggest_parameters(self) -> Dict[str, any]:
+    def make_suggest_parameters(self):
         kwargs = super(ASHA, self).make_suggest_parameters()
+        trial_id = kwargs['trial_id']
 
-        self.rungs[0].add(kwargs['trial_id'])
-        self.update_rungs()
+        sizes = numpy.array([len(b.rungs) for b in self.brackets])
+        probs = numpy.e**(sizes - sizes.max())
+        normalized = probs / probs.sum()
+        idx = numpy.random.choice(len(self.brackets), p=normalized)
+        self.trial_info[trial_id] = self.brackets[idx]
+        self.brackets[idx].register(trial_id)
 
         return kwargs
 
+    def _observe(self, *args, **kwargs):
+        super(ASHA, self)._observe(*args, **kwargs)
+
+        for bracket in self.brackets:
+            bracket.update_rungs()
+
+    def should_suspend(self, trial_id):
+        return self.trial_info[trial_id].should_suspend(trial_id)
+
+    def should_resume(self, trial_id):
+        return not self.should_suspend(trial_id)
+
     def is_completed(self):
-        completed = 0
-        for trial_id in self.rungs.get(self.base, []):
-            last_step, _ = self.get_objective(trial_id)
-            completed += int(last_step >= self.fidelities[-1])
+        return all(bracket.is_completed() for bracket in self.brackets)
 
-            if completed >= self.max_resource:
-                return True
 
-        return False
+class _Bracket():
+    def __init__(self, asha, min_t, max_t, reduction_factor, s):
+        self.asha = asha
+        self.reduction_factor = reduction_factor
+        max_rungs = int(numpy.log(max_t / min_t) / numpy.log(reduction_factor) - s + 1)
+        self.rungs = [(min(min_t * reduction_factor**(k + s), max_t), set())
+                      for k in reversed(range(max_rungs + 1))]
+        if self.rungs[0][0] == self.rungs[1][0]:
+            del self.rungs[0]
+
+        logger.info(f'rungs: {self.rungs}')
+
+    def cutoff(self, recorded):
+        if not recorded:
+            return None
+        return numpy.percentile(list(recorded.values()), (1 - 1 / self.rf) * 100)
 
     def get_rung_id(self, trial_id):
-        for rung_id, rung in sorted(self.rungs.items(), key=lambda item: item[0], reverse=True): 
+        for rung_id, (_, rung) in enumerate(self.rungs):
             if trial_id in rung:
                 return rung_id
 
@@ -68,24 +93,22 @@ class ASHA(HPODispatcher):
 
     def should_suspend(self, trial_id):
         rung_id = self.get_rung_id(trial_id)
-        last_step = max(self.observations[trial_id].keys())
-        return self.fidelities[rung_id] <= last_step
+        last_step = max(self.asha.observations[trial_id].keys())
+        return self.rungs[rung_id][0] <= last_step
 
-    def should_resume(self, trial_id):
-        return not self.should_suspend(trial_id)
+    def register(self, trial_id):
+        self.rungs[-1][1].add(trial_id)
 
     def get_candidate(self, rung_id):
-        rung = self.rungs.get(rung_id, set())
-        next_rung = self.rungs.get(rung_id + 1, set())
-        budget = self.fidelities[rung_id]
-        k = len(rung) // self.reduction_factor
-
+        budget, rung = self.rungs[rung_id]
+        next_rung = self.rungs[rung_id - 1][1]
         completed_trials = []
         for trial_id in rung:
-            last_step, objective = self.get_objective(trial_id)
+            last_step, objective = self.asha.get_objective(trial_id)
             if last_step and last_step >= budget:
                 completed_trials.append((objective, trial_id))
 
+        k = len(completed_trials) // self.reduction_factor
         completed_trials = list(sorted(completed_trials))
         i = 0
         k = min(k, len(completed_trials))
@@ -97,6 +120,15 @@ class ASHA(HPODispatcher):
 
         return None, None
 
+    def is_completed(self):
+        budget, trials = self.rungs[0]
+        for trial_id in trials:
+            last_step, _ = self.asha.get_objective(trial_id)
+            if last_step >= budget:
+                return True
+
+        return False
+
     def update_rungs(self):
         """
 
@@ -107,83 +139,14 @@ class ASHA(HPODispatcher):
             Lookup for promotion in rung l + 1 contains trials of any status.
         """
         # NOTE: There should be base + 1 rungs
-        for rung_id in range(self.base - 1, -1, -1):
+        for rung_id in range(1, len(self.rungs)):
             candidate, objective = self.get_candidate(rung_id)
 
             if candidate:
-                logger.info(f'{candidate} promoting to {rung_id + 1} {objective}')
-                self.rungs[rung_id + 1].add(candidate)
+                logger.info(f'{candidate} promoting to {rung_id - 1} {objective}')
+                self.rungs[rung_id - 1][1].add(candidate)
                 logger.info(
                     'Current rungs: ' +
-                    ' '.join('{}:{}'.format(rung_id, len(rung))
-                             for rung_id, rung in self.rungs.items()))
+                    ' '.join('{}:{}'.format(rung_b, len(rung))
+                             for rung_b, rung in self.rungs))
                 return
-
-
-# asha = ASHA(rung, max_resource=256, min_resource=1, reduction_factor=4,
-#             minimum_early_stopping_rate=0)
-
-def test():
-    parser = argparse.ArgumentParser(description='Script to train a model')
-    parser.add_argument(
-        '--reduction-factor', type=int, required=True,
-        help='Reduction factor for number of workers')
-    parser.add_argument(
-        '--max-resource', default=1, type=int, help='Number of trials to run at full fidelity')
-    parser.add_argument(
-        '--num-workers', type=int, help='Number of workers')
-
-    options = parser.parse_args()
-
-    if options.num_workers is None:
-        options.num_workers = options.max_resource
-
-    import mahler.core.status
-
-    class Space(object):
-        def sample(self, seed=0):
-            return [[random.uniform(0, 1)]]
-
-        def keys(self):
-            return ['a']
-
-    class Task(object):
-        def __init__(self, arguments, output):
-            self.arguments = arguments
-            self.output = output
-            self.status = mahler.core.status.Running('')
-
-    def create_task(arguments, output):
-        return dict(arguments=arguments, output=output, registry=dict(status='Running'))
-
-    fidelities = dict(fidelity='abcd')  # efg')
-
-    space = Space()
-
-    asha = ASHA(
-        space, fidelities,
-        reduction_factor=options.reduction_factor,
-        max_resource=options.max_resource)
-
-    while not asha.final_rung_is_filled():
-        tasks = []
-
-        for i in range(options.num_workers):
-            if asha.final_rung_is_filled():
-                break
-            params = asha.get_params()
-            task = create_task(
-                arguments=params,
-                output=dict(best=dict(valid=dict(error_rate=random.uniform(0, 1)))))
-            tasks.append(task)
-            asha.observe([task])
-
-        for task in tasks:
-            task['registry']['status'] = 'Completed'
-
-        print(" ".join("{}:{}".format(next(iter(fidelities.values()))[key], len(asha.rungs[key]))
-                       for key in sorted(asha.rungs.keys())))
-
-
-if __name__ == "__main__":
-    test()
